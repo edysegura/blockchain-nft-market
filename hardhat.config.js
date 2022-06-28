5bf1e76ce69ad453a8eff743cf78d499fbe1bed4
Edy Segura <edysegura@gmail.com>
2022-06-28 15:27:25 -0300

    chore: add hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+require("@nomiclabs/hardhat-waffle")
+
+const privateKey = process.env.WALLET_PRIVATE_KEY
+const projectId = process.env.INFURA_PROJECT_ID
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mumbai: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    }
+  },
+  solidity: "0.8.4",
+};
