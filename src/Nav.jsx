b51673ae8c336990bd8259701a3e11269e149370
Edy Segura <edysegura@gmail.com>
2022-06-28 21:07:12 -0300

    refactor: nav component

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -8,8 +8,8 @@ class Nav extends Nullstack {
        <nav class="border-b p-6">
         <h1 class="text-4xl font-bold">NFTS for Starving Children</h1>
         <div class="flex mt-4 text-pink-500">
-          <a class="mr-6 text-pink-500" href="/">Home</a>
-          <a href="/">Sell Digital Asset</a>
+          <a class="mr-6" href="/">Home</a>
+          <a class="mr-6" href="/create-item">Sell Digital Asset</a>
         </div>
        </nav>
         <main>
