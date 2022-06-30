b0bd3f10a80964da5b99a4010012b96407ea8681
Edy Segura <edysegura@gmail.com>
2022-06-29 22:41:21 -0300

    feat: upload image to IPFS

--- src/CreateNFT.jsx
+++ src/CreateNFT.jsx
@@ -1,6 +1,28 @@
 import Nullstack from 'nullstack'
 
 class CreateNFT extends Nullstack {
+  name = ''
+  description = ''
+  price = ''
+  fileUrl = ''
+
+  async uploadImageToIPFS({ settings, ipfsClient, event }) {
+    const file = event.target.files[0]
+    try {
+      const added = await ipfsClient.add(file, {
+        progress: (prog) => console.log(`received: ${prog}`),
+      })
+      this.fileUrl = `${settings.ipfsUrl}/${added.path}`
+    } catch (error) {
+      console.log('Error uploading file: ', error)
+    }
+  }
+
+  async testForm() {
+    const { name, description, price, fileUrl } = this
+    console.log({ name, description, price, fileUrl })
+  }
+
   render() {
     return (
       <div class="flex justify-center">
@@ -8,25 +30,29 @@ class CreateNFT extends Nullstack {
           <input
             placeholder="Asset Name"
             class="mt-8 border rounded p-4"
+            bind={this.name}
           />
           <textarea
             placeholder="Asset Description"
             class="mt-2 border rounded p-4"
+            bind={this.description}
           />
           <input
-            placeholder="Asset Price in Eth"
+            placeholder="Asset Price in ETH"
             class="mt-2 border rounded p-4"
+            bind={this.price}
           />
           <input
             type="file"
             name="Asset"
             class="my-4"
+            oninput={this.uploadImageToIPFS}
           />
           {this.fileUrl && (
             <img class="rounded mt-4" width="350" src={this.fileUrl} />
           )}
           <button
-            onclick=""
+            onclick={this.testForm}
             class="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg"
           >
             Create NFT
