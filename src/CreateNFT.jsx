a84cb2bf017bc1bffef7af39e87a67b2507ce857
Edy Segura <edysegura@gmail.com>
2022-06-29 23:21:59 -0300

    feat: implement NFT creation

--- src/CreateNFT.jsx
+++ src/CreateNFT.jsx
@@ -1,4 +1,7 @@
 import Nullstack from 'nullstack'
+import { ethers } from 'ethers'
+import { marketplaceAddress } from '../config'
+import NFTMarketplace from '../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json'
 
 class CreateNFT extends Nullstack {
   name = ''
@@ -16,6 +19,38 @@ class CreateNFT extends Nullstack {
     } catch (error) {
       console.log('Error uploading file: ', error)
     }
+  }
+
+  async uploadToIPFS({ settings, ipfsClient }) {
+    const { name, description, price, fileUrl } = this
+    if (!name || !description || !price || !fileUrl) return
+    const data = JSON.stringify({
+      name,
+      description,
+      image: fileUrl,
+    })
+    try {
+      const added = await ipfsClient.add(data)
+      const url = `${settings.ipfsUrl}/${added.path}`
+      return url
+    } catch (error) {
+      console.log('Error uploading file: ', error)
+    }
+  }
+
+  async listNFTForSale({ router, Web3Modal }) {
+    const url = await this.uploadToIPFS()
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+    const price = ethers.utils.parseUnits(this.price, 'ether')
+    const contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, signer)
+    const listingPrice = await contract.getListingPrice()
+    const priceValue = listingPrice.toString()
+    const transaction = await contract.createToken(url, price, { value: priceValue })
+    await transaction.wait()
+    router.url = '/'
   }
 
   async testForm() {
@@ -52,7 +87,7 @@ class CreateNFT extends Nullstack {
             <img class="rounded mt-4" width="350" src={this.fileUrl} />
           )}
           <button
-            onclick={this.testForm}
+            onclick={this.listNFTForSale}
             class="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg"
           >
             Create NFT
