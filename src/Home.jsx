0a567bfe147115a10679ec4ddb93cf8b5c51f548
Edy Segura <edysegura@gmail.com>
2022-06-29 23:22:55 -0300

    fix: NFT list at home

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,18 +4,11 @@ import { marketplaceAddress } from '../config'
 import NFTMarketplace from '../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json'
 
 class Home extends Nullstack {
-  _Web3Modal = null
   nftItems = []
   loading = true
 
   async hydrate() {
     this.nftItems = await this.loadNFTs()
-    this._Web3Modal = await this.loadWeb3Modal()
-  }
-
-  async loadWeb3Modal() {
-    const Web3Modal = await import('web3modal')
-    return Web3Modal
   }
 
   async fetchData({ uri }) {
@@ -26,7 +19,6 @@ class Home extends Nullstack {
 
   async loadNFTs() {
     this.loading = true
-    /* create a generic provider and query for unsold market items */
     const provider = new ethers.providers.JsonRpcProvider()
     const contract = new ethers.Contract(
       marketplaceAddress,
@@ -51,9 +43,9 @@ class Home extends Nullstack {
           tokenId: marketItem.tokenId.toNumber(),
           seller: marketItem.seller,
           owner: marketItem.owner,
-          image: meta.data.image,
-          name: meta.data.name,
-          description: meta.data.description,
+          image: meta.image,
+          name: meta.name,
+          description: meta.description,
         }
         return item
       }),
@@ -62,8 +54,8 @@ class Home extends Nullstack {
     return items
   }
 
-  async buyNFT({ nft }) {
-    const web3Modal = new this._Web3Modal()
+  async buyNFT({ Web3Modal, nft }) {
+    const web3Modal = new Web3Modal()
     const connection = await web3Modal.connect()
     const provider = new ethers.providers.Web3Provider(connection)
     const signer = provider.getSigner()
@@ -85,11 +77,11 @@ class Home extends Nullstack {
       return <h1 class="px-20 py-10 text-3xl">No items in marketplace</h1>
     return (
       <section class="flex justify-center">
-        <div class="px-4 max-w-[1600px]">
+        <div class="px-4">
           <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
             {this.nftItems.map((nft) => (
               <div class="border shadow rounded-xl overflow-hidden">
-                <img src={nft.image} />
+                <img class="w-full" src={nft.image} alt={nft.name} />
                 <div class="p-4">
                   <p class="text-2xl h-[64px] font-semibold">{nft.name}</p>
                   <div class="h-[70px] overflow-hidden">
@@ -100,7 +92,7 @@ class Home extends Nullstack {
                   <p class="text-2xl font-bold text-white">{nft.price} ETH</p>
                   <button
                     class="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
-                    onclick={() => this.buyNft(nft)}
+                    onclick={() => this.buyNFT({ nft })}
                   >
                     Buy
                   </button>
