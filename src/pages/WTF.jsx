fc58941b8eb940a42b5f01e64ebd706c5afb5810
Edy Segura <edysegura@gmail.com>
2022-07-05 10:37:31 -0300

    feat: implement the top content of WTF page

--- src/pages/WTF.jsx
+++ src/pages/WTF.jsx
@@ -1,19 +1,25 @@
 import Nullstack from 'nullstack'
+import WalletIcon from '../icons/wallet'
 import GreatPowerFooter from '../layout/greatPowerFooter'
 import Title from '../layout/title'
 
 class WTF extends Nullstack {
   renderTopContent() {
     return (
-      <section>
+      <section class="m-auto flex w-80 flex-col justify-center text-center">
         <Title
           text="Treat your charity like your investments"
           highlightedText="- Expect ROI"
         />
-        <p>
+        <p class="mb-10">
           Bringing attention to effective altruism, and reminding you that not
           all charities are created equal
         </p>
+        <button class="m-auto w-60 bg-mustard p-3 font-extrabold text-black">
+          <div class="flex items-center justify-center">
+            <WalletIcon /> <span class="ml-3">Connect your wallet</span>
+          </div>
+        </button>
       </section>
     )
   }
