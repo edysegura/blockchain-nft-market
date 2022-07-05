bbc2018cfa67909d5bd573bb49d92e1694f5395e
Edy Segura <edysegura@gmail.com>
2022-07-05 14:16:51 -0300

    feat: implement the middle content in WTF page

--- src/pages/WTF.jsx
+++ src/pages/WTF.jsx
@@ -6,7 +6,7 @@ import Title from '../layout/title'
 class WTF extends Nullstack {
   renderTopContent() {
     return (
-      <section class="m-auto flex w-80 flex-col justify-center text-center">
+      <section class="m-auto mb-48 flex w-80 flex-col justify-center text-center">
         <Title
           text="Treat your charity like your investments"
           highlightedText="- Expect ROI"
@@ -24,10 +24,43 @@ class WTF extends Nullstack {
     )
   }
 
+  renderMiddleContent() {
+    return (
+      <section>
+        <div class="m-auto flex w-[500px] flex-col justify-center gap-5 text-center">
+          <Title text="Giving to charities that count" />
+          <p>
+            Some “charities” manipulate emotions with imagery. This helps
+            fundraiser, but not necessarily those in need. Treat your charity
+            like any other investment. Verify that your hard-earned money
+            benefits those in greatest need. Know what percentage of each
+            donation is retained vs. distributed!
+          </p>
+          <p>
+            This project brings awareness to the issue of inefficient charities
+            that keep most of the funds they raise internally. Let's help
+            altruistic people give to the most efficient, effective charities!
+          </p>
+          <p>
+            Note: All monetary proceeds from this project will be donated to
+            charities deemed effective by GiveWell.
+          </p>
+          <a
+            href="https://www.givingwhatwecan.org/best-charities-to-donate-to-2022"
+            class="m-auto w-64 bg-mustard p-2 text-center font-extrabold text-black"
+          >
+            Givin what we can
+          </a>
+        </div>
+      </section>
+    )
+  }
+
   render() {
     return (
       <>
         <TopContent />
+        <MiddleContent />
         <GreatPowerFooter />
       </>
     )
