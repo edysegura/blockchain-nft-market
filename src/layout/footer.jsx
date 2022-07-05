372a77f2aeb07ed4526897c141d04625ff6f54f5
Edy Segura <edysegura@gmail.com>
2022-07-04 21:49:50 -0300

    chore: add tailwind prettier plugin

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -1,29 +1,45 @@
 export default function Footer() {
   return (
-    <footer class="mt-5 pt-8 pb-8 border-t border-white">
-      <div class="w-11/12 flex justify-between">
+    <footer class="mt-5 border-t border-white pt-8 pb-8">
+      <div class="flex w-11/12 justify-between">
         <div class="flex flex-col">
           <div class="uppercase text-white">
             <div class="text-center">
-              <span class="block font-bold text-sm">NFTS For</span>
-              <span class="block font-extralight text-2xl">Starving</span>
-              <span class="block font-extralight text-2xl">Children</span>
+              <span class="block text-sm font-bold">NFTS For</span>
+              <span class="block text-2xl font-extralight">Starving</span>
+              <span class="block text-2xl font-extralight">Children</span>
             </div>
           </div>
-          <p class="w-56 mt-10 ml-10 text-white">
+          <p class="mt-10 ml-10 w-56 text-white">
             They may not have food, but you can help an NFT-less child with this
             buy one, give one opportunity. Every child deserves an NFT.
           </p>
         </div>
         <nav>
-          <h2 class="font-bold text-lg mb-5">Marketplace</h2>
+          <h2 class="mb-5 text-lg font-bold shadow-white drop-shadow-[0_0_10px_rgba(255,255,255,0.7)]">
+            Marketplace
+          </h2>
           <ul>
             <li class="mb-3">WFT?</li>
-            <li class="mb-3">Explore</li>
+            <li class="mb-3">
+              <a href="/explore">Explore</a>
+            </li>
             <li class="mb-3">TAPs</li>
-            <li><a class="text-black hover:text-mustard" href="/create-nft">Create NFT</a></li>
-            <li><a class="text-black hover:text-mustard" href="/my-nfts">My NFT</a></li>
-            <li><a class="text-black hover:text-mustard" href="/dashboard">Dashboard</a></li>
+            <li>
+              <a class="text-black hover:text-mustard" href="/create-nft">
+                Create NFT
+              </a>
+            </li>
+            <li>
+              <a class="text-black hover:text-mustard" href="/my-nfts">
+                My NFT
+              </a>
+            </li>
+            <li>
+              <a class="text-black hover:text-mustard" href="/dashboard">
+                Dashboard
+              </a>
+            </li>
           </ul>
         </nav>
       </div>
