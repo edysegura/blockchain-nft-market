474b9da91f26ddde12e6411fecc933055f15b8ef
Edy Segura <edysegura@gmail.com>
2022-07-05 09:46:51 -0300

    feat: implement the highlight in top nav menu links

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,9 +2,15 @@ import Nullstack from 'nullstack'
 import { AccountIcon } from '../icons/account'
 
 class Header extends Nullstack {
+  currentRouterStyle({ router, linkHref }) {
+    if (router.url === linkHref) {
+      return 'border-b-2 border-mustard'
+    }
+  }
+
   renderMyAccount() {
     return (
-      <a href='#' class="flex justify-end items-center pt-3">
+      <a href="#" class="flex items-center justify-end pt-3">
         <AccountIcon /> <span class="ml-2">My Account</span>
       </a>
     )
@@ -15,22 +21,29 @@ class Header extends Nullstack {
       <div>
         <MyAccount />
         <div class="flex items-center justify-between p-6">
-          <h1 class="w-[175px] pt-5 pl-5 text-sm text-center uppercase text-white">
+          <h1 class="w-[175px] pt-5 pl-5 text-center text-sm uppercase text-white">
             <div class="font-bold">NFTS For</div>
-            <div class="font-extralight text-2xl">Starving<br />Children</div>
+            <div class="text-2xl font-extralight">
+              Starving
+              <br />
+              Children
+            </div>
           </h1>
           <nav>
-            <div class="flex mt-4 text-white justify-end">
-              <a class="mr-6 border-b-2 border-mustard" href="/">
+            <div class="mt-4 flex justify-end text-white">
+              <a
+                class={`mr-6 ${this.currentRouterStyle({ linkHref: '/' })}`}
+                href="/"
+              >
                 Home
               </a>
-              <a class="mr-6" href="/wtf">
+              <a class={`mr-6 ${this.currentRouterStyle({ linkHref: '/wtf' })}`} href="/wtf">
                 WFT?
               </a>
-              <a class="mr-6" href="/explore">
+              <a class={`mr-6 ${this.currentRouterStyle({ linkHref: '/explore' })}`} href="/explore">
                 Explore
               </a>
-              <a class="mr-6" href="/taps">
+              <a class={`mr-6 ${this.currentRouterStyle({ linkHref: '/taps' })}`} href="/taps">
                 TAPs
               </a>
             </div>
