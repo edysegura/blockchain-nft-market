0f083dd2b0f5e4d57b5892d11415023891705dd3
Edy Segura <edysegura@gmail.com>
2022-07-05 14:16:19 -0300

    feat: enhance title component

--- src/layout/title.jsx
+++ src/layout/title.jsx
@@ -4,9 +4,11 @@ export default function Title({ text, highlightedText }) {
       <span class="shadow-white drop-shadow-[0_0_10px_rgba(255,255,255,0.7)]">
         {text}{' '}
       </span>
-      <span class="bg-mustard p-2 text-black shadow-black drop-shadow-none">
-        {highlightedText}
-      </span>
+      {highlightedText && (
+        <span class="bg-mustard p-2 text-black shadow-black drop-shadow-none">
+          {highlightedText}
+        </span>
+      )}
     </h2>
   )
 }
