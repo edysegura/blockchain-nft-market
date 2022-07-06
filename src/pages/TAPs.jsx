4c7324436ca18ac5115dd7db6468c0932d8c943e
Edy Segura <edysegura@gmail.com>
2022-07-06 10:57:10 -0300

    refactor: remove additional input

--- src/pages/TAPs.jsx
+++ src/pages/TAPs.jsx
@@ -32,13 +32,15 @@ class TAPs extends Nullstack {
       <section>
         <div class="m-auto w-[500px] text-center">
           <Title text="Buy TAPs" />
-          <div class="flex flex-col gap-3 border p-10 bg-black border-gray-800">
-            <div>
-              <input class="w-full border bg-black p-2 border-white text-white text-2xl font-extrabold" value="0.00" />
-            </div>
+          <div class="flex flex-col gap-3 border border-gray-800 bg-black p-10">
             <div class="relative">
-              <input class="w-full border bg-black p-2 pr-20 border-white text-white text-2xl font-extrabold" value="0.00" />
-              <span class="flex justify-center items-center gap-2 text-sm absolute right-4 top-4"><TapIcon /> TAPs</span>
+              <input
+                class="w-full border border-white bg-black p-2 pr-20 text-2xl font-extrabold text-white"
+                value="0.00"
+              />
+              <span class="absolute right-4 top-4 flex items-center justify-center gap-2 text-sm">
+                <TapIcon /> TAPs
+              </span>
             </div>
             <button class="m-auto w-60 bg-mustard p-3 font-extrabold text-black">
               <div class="flex items-center justify-center">
