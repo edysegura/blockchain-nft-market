fa152a642a9d1ea887c23c8717a49fcdc68b6ece
Edy Segura <edysegura@gmail.com>
2022-07-05 22:43:35 -0300

    feat: implement buy taps form layout

--- src/pages/TAPs.jsx
+++ src/pages/TAPs.jsx
@@ -1,6 +1,8 @@
 import Nullstack from 'nullstack'
+import TapIcon from '../icons/tap'
 import WalletIcon from '../icons/wallet'
 import GreatPowerFooter from '../layout/greatPowerFooter'
+import Title from '../layout/title'
 
 class TAPs extends Nullstack {
   renderTopContent() {
@@ -25,10 +27,35 @@ class TAPs extends Nullstack {
     )
   }
 
+  renderBuyTap() {
+    return (
+      <section>
+        <div class="m-auto w-[500px] text-center">
+          <Title text="Buy TAPs" />
+          <div class="flex flex-col gap-3 border p-10 bg-black border-gray-800">
+            <div>
+              <input class="w-full border bg-black p-2 border-white text-white text-2xl font-extrabold" value="0.00" />
+            </div>
+            <div class="relative">
+              <input class="w-full border bg-black p-2 pr-20 border-white text-white text-2xl font-extrabold" value="0.00" />
+              <span class="flex justify-center items-center gap-2 text-sm absolute right-4 top-4"><TapIcon /> TAPs</span>
+            </div>
+            <button class="m-auto w-60 bg-mustard p-3 font-extrabold text-black">
+              <div class="flex items-center justify-center">
+                <WalletIcon /> <span class="ml-3">Connect your wallet</span>
+              </div>
+            </button>
+          </div>
+        </div>
+      </section>
+    )
+  }
+
   render() {
     return (
       <>
         <TopContent />
+        <BuyTap />
         <GreatPowerFooter />
       </>
     )
