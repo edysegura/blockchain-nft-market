f69be5f714706f890de98ee91cad14970eb10fdd
Edy Segura <edysegura@gmail.com>
2022-07-05 21:21:27 -0300

    style: top content in TAPs page

--- src/pages/TAPs.jsx
+++ src/pages/TAPs.jsx
@@ -1,16 +1,26 @@
 import Nullstack from 'nullstack'
+import WalletIcon from '../icons/wallet'
 import GreatPowerFooter from '../layout/greatPowerFooter'
-import Title from '../layout/title'
 
 class TAPs extends Nullstack {
   renderTopContent() {
     return (
-      <section>
-        <Title text="Meet our token" highlightedText="Toughts and Prayers" />
-        <p>
+      <section class="m-auto mb-48 flex w-80 flex-col justify-center text-center">
+        <h2 class="text-2xl font-extrabold shadow-white drop-shadow-[0_0_10px_rgba(255,255,255,0.7)]">
+          Meet our token
+        </h2>
+        <p class="mb-6 mt-3 bg-mustard p-2 text-2xl font-extrabold text-black shadow-black drop-shadow-none">
+          Toughts and Prayers
+        </p>
+        <p class="mb-10">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
+        <button class="m-auto w-60 bg-mustard p-3 font-extrabold text-black">
+          <div class="flex items-center justify-center">
+            <WalletIcon /> <span class="ml-3">Connect your wallet</span>
+          </div>
+        </button>
       </section>
     )
   }
