c4ff00bc0d50926943c28bc60a4be01d42ad6720
Edy Segura <edysegura@gmail.com>
2022-07-06 16:18:00 -0300

    feat: add admin layout baseline

--- src/Admin/Admin.jsx
+++ src/Admin/Admin.jsx
@@ -1,4 +1,6 @@
 import Nullstack from 'nullstack'
+import ButtonLink from '../layout/buttonLink'
+import NftSlogan from '../layout/nftSlogan'
 import Title from '../layout/title'
 import CreateNFT from './CreateNFT'
 import Dashboard from './Dashboard'
@@ -8,13 +10,23 @@ import ResellNFT from './ResellNFT'
 class Admin extends Nullstack {
   render() {
     return (
-      <div>
-        <Title text="Faucet" />
-        <CreateNFT route="/admin/create-nft" />
-        <MyNFTs route="/admin/my-nfts" />
-        <Dashboard route="/admin/dashboard" />
-        <ResellNFT route="/admin/resell-nft" />
-      </div>
+      <main class="min-h-screen w-full max-w-7xl 2xl:mx-auto flex">
+        <nav class="h-full min-h-screen w-1/5 bg-black">
+          <header class="flex flex-col items-center gap-5">
+            <NftSlogan />
+            <ButtonLink clazz="w-32" href="/admin/create-nft">
+              Create NFT
+            </ButtonLink>
+          </header>
+        </nav>
+        <section class="w-4/5">
+          <Title text="Faucet" />
+          <CreateNFT route="/admin/create-nft" />
+          <MyNFTs route="/admin/my-nfts" />
+          <Dashboard route="/admin/dashboard" />
+          <ResellNFT route="/admin/resell-nft" />
+        </section>
+      </main>
     )
   }
 }
