69d2740b78c53d3401fc49a5bbe508d5937e980a
Edy Segura <edysegura@gmail.com>
2022-07-07 16:20:46 -0300

    feat: add placeholder for upload

--- src/layout/label.jsx
+++ src/layout/label.jsx
@@ -1,6 +1,6 @@
 export default function Label({ children }) {
   return (
-    <label class="text-xl font-extrabold shadow-white drop-shadow-[0_0_10px_rgba(255,255,255,0.7)]">
+    <label class="text-lg font-extrabold shadow-white drop-shadow-[0_0_10px_rgba(255,255,255,0.7)]">
       {children}
     </label>
   )
