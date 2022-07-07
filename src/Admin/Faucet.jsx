a8245f9152dd036a5320823013cc5b6dce6adb9e
Edy Segura <edysegura@gmail.com>
2022-07-07 12:09:52 -0300

    feat: implement text control and label component

--- src/Admin/Faucet.jsx
+++ src/Admin/Faucet.jsx
@@ -1,15 +1,37 @@
 import Nullstack from 'nullstack'
 import FaucetIcon from '../icons/faucet'
+import TapIcon from '../icons/tap'
+import Label from '../layout/label'
 import SimpleTitle from '../layout/simpleTitle'
+import TextControl from '../layout/textControl'
 import Title from '../layout/title'
 
 class Faucet extends Nullstack {
   render() {
     return (
       <section class="pl-36 pt-14">
-        <div class="flex items-center gap-3">
+        <div class="mb-10 flex items-center gap-3">
           <FaucetIcon />
           <SimpleTitle>Faucet</SimpleTitle>
+        </div>
+        <div class="flex w-1/2 flex-col gap-10">
+          <div>
+            <TextControl
+              label="Address"
+              placeholder="Enter your Wallet address (0x...) or ENS Domain"
+            />
+          </div>
+          <div class="flex justify-between">
+            <div class="flex flex-col">
+              <Label>Quantity</Label>
+              <span class="text-gray-400">
+                Put the quantity of TAPs you'll need
+              </span>
+            </div>
+            <div>
+              <TapIcon />
+            </div>
+          </div>
         </div>
       </section>
     )
---
+++ src/layout/label.jsx
@@ -0,0 +1,7 @@
+export default function Label({ children }) {
+  return (
+    <label class="text-xl font-extrabold shadow-white drop-shadow-[0_0_10px_rgba(255,255,255,0.7)]">
+      {children}
+    </label>
+  )
+}
