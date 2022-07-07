1097204f09756d018cb862c3892eb0c06e90f50f
Edy Segura <edysegura@gmail.com>
2022-07-07 15:22:18 -0300

    feat: implement NFTs pages

--- src/layout/nftGrid.jsx
+++ src/layout/nftGrid.jsx
@@ -3,7 +3,7 @@ import NftCard from './nftCard'
 export default function NftGrid({ nfts }) {
   return (
     <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
-      {nfts.map((nft) => (
+      {nfts.length > 0 && nfts.map((nft) => (
         <NftCard nft={nft} />
       ))}
     </div>
