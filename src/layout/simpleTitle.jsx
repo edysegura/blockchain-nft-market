8d991a144aebd3370f2dcf9c6eadaed34c474803
Edy Segura <edysegura@gmail.com>
2022-07-07 11:16:59 -0300

    feat: implement simple title component

---
+++ src/layout/simpleTitle.jsx
@@ -0,0 +1,7 @@
+export default function SimpleTitle({ children }) {
+  return (
+    <h2 class="text-3xl font-extrabold shadow-white drop-shadow-[0_0_10px_rgba(255,255,255,0.7)]">
+      {children}
+    </h2>
+  )
+}
